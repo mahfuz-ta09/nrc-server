92b10f71415914c82cc0071fd6aadd09b4276817
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-02-04 16:56:32 +0600

    test push done

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -10,6 +10,8 @@ const corsOption = {
         }
     },
     credentials: true,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
     optionSuccessStatus: 200
 }
 
