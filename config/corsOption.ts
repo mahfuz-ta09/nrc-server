7acf6f1ac780cbb0506e796c2fa53e01bda2cd06
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-02-21 23:13:09 +0600

    cors changed1.4

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -4,13 +4,7 @@ const allowedOrigins = [
 ]
 
 const corsOption = {    
-    origin: (origin: string | undefined, callback: any) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not Allowed by CORS"));
-        }
-    },
+    origin: 'https://nrc-london.vercel.app',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: ["Content-Type", "Authorization","authorization"],
