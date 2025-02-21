384b7a4474e689ba28d2d40dac7e269e1996b60c
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-02-21 15:34:11 +0600

    cors changed1.1

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -4,7 +4,13 @@ const allowedOrigins = [
 ]
 
 const corsOption = {    
-    origin: 'http://localhost:3000',
+    origin: (origin: string | undefined, callback: any) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not Allowed by CORS"));
+        }
+    },
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: ["Content-Type", "Authorization","authorization"],
