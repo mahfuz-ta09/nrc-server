8c8472373f88aaf4cae5bb4a6c13357c31b215f7
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-03-09 03:30:32 +0600

    origin changed

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -1,8 +1,9 @@
 
+// origin: 'https://nrc-london.vercel.app',
+// origin: 'http://localhost:3000',
 
 const corsOption = {    
-    origin: 'https://nrc-london.vercel.app',
-    // origin: 'http://localhost:3000',
+    origin: 'https://www.nrcedu-uk.com/',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: ["Content-Type", "Authorization","authorization"],
