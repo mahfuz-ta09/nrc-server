3a3ae76435b612968c8f4f6c5967cdf69856f776
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-03-09 03:34:28 +0600

    origin changed1.1

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -1,9 +1,16 @@
 
 // origin: 'https://nrc-london.vercel.app',
 // origin: 'http://localhost:3000',
-
+// const allowedOrigins = ['https://www.nrcedu-uk.com', 'https://subdomain.nrcedu-uk.com']
+// origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//     } else {
+//         callback(new Error('Not allowed by CORS'));
+//     }
+// },
 const corsOption = {    
-    origin: 'https://www.nrcedu-uk.com/',
+    origin: 'https://www.nrcedu-uk.com',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: ["Content-Type", "Authorization","authorization"],
