fa12739d00e1c0fae94a19d78f1fb7c8f0581c08
mahfuz-ta09 <mahfuz.ta09@gmail.com>
2025-07-25 15:14:02 +0600

    origin changed

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -6,14 +6,14 @@ const urlList = [
 
 
 const corsOption = {    
-    // origin: function (origin:any, callback:any) {
-    //   if (urlList.indexOf(origin) !== -1) {
-    //     callback(null, true)
-    //   } else {
-    //     callback(new Error('Not allowed by CORS'))
-    //   }
-    // },
-    origin:'https://www.nrcedu-uk.com',
+    origin: function (origin:any, callback:any) {
+      if (urlList.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    // origin:'https://www.nrcedu-uk.com',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
     allowedHeaders: ["Content-Type", "Authorization","authorization"],
