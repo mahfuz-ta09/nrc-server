d439351f7b1edbb755acc079aab0b2060d4c182d
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-09-04 15:51:42 +0600

    cookie issue

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -1,18 +1,17 @@
 const urlList = [
-    'http://localhost:3000',
-    'https://nrc-london.vercel.app',
-    'https://www.nrcedu-uk.com',
+    "http://localhost:3000",
+    "https://nrc-london.vercel.app",
+    "https://nrcedu-uk.com",
+    "https://www.nrcedu-uk.com",
     'https://api.nrcedu-uk.com',
-] 
-
-
+]
 const corsOption = {    
-    origin: function (origin:any, callback:any) {
-      if (urlList.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+    origin: function (origin: any, callback: any) {
+        if (!origin || urlList.includes(origin)) {
+          callback(null, true)
+        } else {
+          callback(new Error("Not allowed by CORS"))
+        }
     },
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS,PATCH',
--- helper/sendEmail.ts
+++ helper/sendEmail.ts
@@ -11,6 +11,9 @@ const sendEmail = async (to: string, subject: string, htmlContent: string) => {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
+            tls: {
+                rejectUnauthorized: false, // allow self-signed locally
+            },
         })
 
         const mailOptions = {
